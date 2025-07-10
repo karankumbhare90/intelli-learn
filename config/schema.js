e0a58da1a24adf0efb75d3385b9ca0c025f9e2d8
Karan Kumbhare <karankumbhare90@gmail.com>
2025-07-10 22:40:08 +0530

    Create a course Layout

--- config/schema.js
+++ config/schema.js
@@ -1,12 +1,23 @@
 
-import { integer, pgTable, varchar, timestamp } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, timestamp, json, boolean } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
     id: integer().primaryKey().generatedAlwaysAsIdentity(),
     name: varchar({ length: 255 }).notNull(),
     email: varchar({ length: 255 }).notNull().unique(),
     profileImage: varchar({ length: 255 }),
-    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
-    updatedAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
     subscriptionId: varchar()
 });
+
+export const coursesTable = pgTable("courses", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    cid: varchar().notNull(),
+    name: varchar().notNull(),
+    description: varchar(),
+    noOfChapters: integer().notNull(),
+    includeVideo: boolean().default(false),
+    level: varchar().notNull(),
+    category: varchar(),
+    courseJSON: json(),
+    userEmail: varchar('userEmail').references(() => usersTable.email),
+})
