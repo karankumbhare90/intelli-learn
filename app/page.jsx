a2b4dc5e3a89786d690aaa9e23372d9e02b404bb
Karan Kumbhare <karankumbhare90@gmail.com>
2025-07-19 19:06:27 +0530

    Implement Simple Landing Page

--- app/page.jsx
+++ app/page.jsx
@@ -1,15 +1,34 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
-      <div className="h-screen flex flex-col items-center justify-center gap-2 mt-2.5">
-        <div className="flex items-center justify-center gap-2 mt-2.5">
-          <h1>Hello Developer 👨‍💻</h1> <Button>Get Started</Button>
+      <div className="h-screen w-full">
+        <div className="w-full h-full flex items-center justify-center">
+          <div className="w-full h-full mx-auto container sm:px-0">
+            <div className="w-full gap-5 h-full flex flex-col items-center justify-center">
+              <h1 className="text-4xl font-bold text-center">Welcome to Intelli Learn</h1>
+
+              <Image
+                src={'/learning.jpg'}
+                alt="Banner"
+                height={450}
+                width={600}
+                className="w-3/4 max-h-[400px] object-cover rounded-2xl"
+              />
+
+              <Link href={'/workspace'}>
+                <Button>
+                  Go to Dashboard
+                </Button>
+              </Link>
+            </div>
+          </div>
         </div>
-        <UserButton />
-      </div>
+      </div >
 
     </>
   );
